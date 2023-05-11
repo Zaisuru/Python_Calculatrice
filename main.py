15ab37c7e75e1ad69ffea7a2472de08507743c0f
Zaisuru <thomas.holderbaum@gmail.com>
2023-05-11 15:42:38 +0200

    Resultat

---
+++ main.py
@@ -0,0 +1,7 @@
+# Creation d'une calculatrice sur python en ligne de commande
+PremierNombre = input("Veuillez entrer un premier nombre :")
+DeuxiemeNombre = input("Veuillez entrer un deuxième nombre :")
+
+resultat = int(PremierNombre) + int(DeuxiemeNombre)
+
+print (f"Le résultat de l'addition du nombre {PremierNombre} avec le nombre {DeuxiemeNombre} est égal à {resultat}")
