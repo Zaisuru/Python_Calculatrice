f3e4d0badd11f4327a7db16aac88096c90ab455c
Zaisuru <thomas.holderbaum@gmail.com>
2023-05-12 10:57:34 +0200

    correction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Creation d'une calculatrice sur python en ligne de commande
-PremierNombre = input("Veuillez entrer un premier nombre :")
-DeuxiemeNombre = input("Veuillez entrer un deuxième nombre :")
 
-print (f"Le résultat de l'addition du nombre {PremierNombre} avec le nombre {DeuxiemeNombre} est égal à {int(PremierNombre) + int(DeuxiemeNombre)}")
+first_number = input("Veuillez entrer un premier nombre :")
+second_number = input("Veuillez entrer un deuxième nombre :")
+
+print(f"Le résultat de l'addition du nombre {first_number} avec le nombre {second_number} est égal à {int(first_number) + int(second_number)}")
