8bbcc17e0534e2b1fede21317ef08a15a6c798e0
Zaisuru <thomas.holderbaum@gmail.com>
2023-05-12 10:41:42 +0200

    Armand tu arrête

--- main.py
+++ main.py
@@ -2,6 +2,4 @@
 PremierNombre = input("Veuillez entrer un premier nombre :")
 DeuxiemeNombre = input("Veuillez entrer un deuxième nombre :")
 
-resultat = int(PremierNombre) + int(DeuxiemeNombre)
-
-print (f"Le résultat de l'addition du nombre {PremierNombre} avec le nombre {DeuxiemeNombre} est égal à {resultat}")
+print (f"Le résultat de l'addition du nombre {PremierNombre} avec le nombre {DeuxiemeNombre} est égal à {int(PremierNombre) + int(DeuxiemeNombre)}")
