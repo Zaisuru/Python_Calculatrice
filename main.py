ee102706ea4e42001a69f5d6630e6f9bfc79b5ae
Zaisuru <thomas.holderbaum@gmail.com>
2023-06-01 12:42:46 +0200

    Exercice 2: Gestion des erreurs dans une calculatrice

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 # Creation d'une calculatrice sur python en ligne de commande
 
-first_number = input("Veuillez entrer un premier nombre :")
-second_number = input("Veuillez entrer un deuxième nombre :")
+a = b = ""
 
-print(f"Le résultat de l'addition du nombre {first_number} avec le nombre {second_number} est égal à {int(first_number) + int(second_number)}")
+while not (a.isdigit() and b.isdigit()):
+    a = input("Veuillez entrer un premier nombre")
+    b = input("Veuillez entrer un deuxieme nombre")
+
+    if not (a.isdigit() and b.isdigit()):
+        print("Veuillez entrer un nombre valide")
+print (f"Le result de {a} et de {b} est égal à {int(a) + int(b)}")
